eb85b254ae570ff4d2422f1c76174d7ab9102f60
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-02-06 16:03:04 +0100

    store results into file live.json

---
+++ DataRecorder/liveUpdater.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const pathLiveDataJson = "Data/live.json";
+//const pathLiveDataJsonPTDR = "Data/livePtdr.json";
+
+const liveData = {
+  name: "piensg028",
+  status:true,
+  location: {
+    date: null,
+    coords: [null, null]
+  },
+  measurements: {
+    date: null,
+    temperature: null,
+    humidity: null,
+    presure: null,
+    rain: null,
+    light: null,
+    wind: {
+      speed: null,
+      direction: null,
+    }
+  }
+};
+
+function writeJsonFile(path, data) {
+  fs.writeFile(path, JSON.stringify(data), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function getDataLivePTDR() {
+  const livePTDR = {...liveData};
+  return livePTDR;
+}
+
+function updateLive() {
+  writeJsonFile(pathLiveDataJson, liveData);
+  //writeJsonFile(pathLiveDataJsonPTDR, getDataLivePTDR());
+}
+
+updateLive();
+
+module.exports = {
+  liveData: liveData,
+  updateLive: updateLive
+};
--- DataRecorder/recorder.js
+++ DataRecorder/recorder.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 require('log-timestamp');
+const lu = require("./liveUpdater.js");
+
+const dir_simul = true ? "/home/formation/Bureau/simulPi" : "";
 
 function watchFile(path, callback, callbackError = console.error) {
   fs.watchFile(path, {interval: 10}, (curr, prev) => {
@@ -12,14 +15,16 @@ function watchFile(path, callback, callbackError = console.error) {
   });
 }
 
-const dir_simul = true ? "/home/formation/Bureau/simulPi" : "";
-
 const pathDataSensHat = dir_simul + "/dev/shm/tph.log";
 function readFileTph(fileContent) {
   const parsed = JSON.parse(fileContent);
   parsed.date = new Date(parsed.date);
-  console.log("updated: tph.log");
-  console.log("data = ", parsed);
+
+  lu.liveData.measurements.date = parsed.date;
+  lu.liveData.measurements.temperature = parsed.temp;
+  lu.liveData.measurements.humidity = parsed.hygro;
+  lu.liveData.measurements.presure = parsed.press;
+  lu.updateLive();
 }
 watchFile(pathDataSensHat, readFileTph);
 
@@ -27,8 +32,9 @@ watchFile(pathDataSensHat, readFileTph);
 const pathDataRain = dir_simul + "/dev/shm/rainCounter.log";
 function readFileRain(fileContent) {
   const date = new Date(fileContent);
-  console.log("updated: rainCounter.log");
-  console.log("date = ", date);
+
+  lu.liveData.measurements.rain = date;
+  lu.updateLive();
 }
 watchFile(pathDataRain, readFileRain);
 
@@ -41,19 +47,24 @@ function readFileGPS(fileContent) {
   const trameSpl = trame.split(",");
   const data = {
     date: new Date(),
-    lat: parseFloat(trameSpl[2]),
-    latDirection: trameSpl[3],
-    lng: parseFloat(trameSpl[4]),
-    lngDirection: trameSpl[5],
+    lat: parseFloat(trameSpl[2]) * (trameSpl[3] == "N" ? 1 : -1),
+    lng: parseFloat(trameSpl[4]) * (trameSpl[5] == "E" ? 1 : -1),
+    latGeo: null,
+    lngGeo: null,
     precision: parseFloat(trameSpl[8]),
     altitude: parseFloat(trameSpl[9]),
   };
+  data.latGeo = Math.round((parseInt(data.lat/100) + (data.lat % 100)/60)*1e6) / 1e6;
+  data.lngGeo = Math.round((parseInt(data.lng/100) + (data.lng % 100)/60)*1e6) / 1e6;
+
   const time = parseFloat(trameSpl[1]);
   data.date.setHours(parseInt(time / 10000)+1);
   data.date.setMinutes(parseInt(time % 10000 / 100));
   data.date.setSeconds(Math.round(time % 100));
-  console.log("updated: gpsNmea");
-  console.log("data = ", data);
+
+  lu.liveData.location.date = data.date;
+  lu.liveData.location.coords = [data.latGeo, data.lngGeo];
+  lu.updateLive();
 }
 watchFile(pathDataGPS, readFileGPS);
 
@@ -61,10 +72,18 @@ const pathDataSensors = dir_simul + "/dev/shm/sensors";
 function readFileSensors(fileContent) {
   const parsed = JSON.parse(fileContent);
   parsed.date = new Date(parsed.date);
+  parsed.measurements = {};
   parsed.measure.forEach(measure => {
     measure.value = parseFloat(measure.value);
+    parsed.measurements[measure.name] = measure.value;
   });
-  console.log("updated: sensors");
-  console.log("data = ", parsed);
+
+  lu.liveData.measurements.date = parsed.date;
+  lu.liveData.measurements.light = parsed.luminosity;
+  lu.liveData.measurements.wind = {
+    speed: parsed.measurements.wind_speed_avg,
+    direction: parsed.measurements.wind_heading,
+  };
+  lu.updateLive();
 }
 watchFile(pathDataSensors, readFileSensors);
