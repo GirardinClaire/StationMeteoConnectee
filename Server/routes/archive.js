69dd95e3937bb9c725d0000aca5796880805eb72
Claire_GRD <claire-girardin@hotmail.fr>
2024-02-06 16:25:56 +0100

    archive server

--- Server/routes/archive.js
+++ Server/routes/archive.js
@@ -58,13 +58,49 @@ const db = require('../database.js');
 //   return formattedData;
 // }
 
+// /* GET Archive data */
+// router.get('/', async function(req, res, next) {
+//   const { from, to, filter, interval } = req.query;
+//   try {
+//     const archiveData = await getArchiveData(from, to, filter, interval);
+//     res.json(archiveData);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+
+
+async function getArchiveData() {
+  // Retourne un objet JSON vide avec la structure spécifique
+  return {
+    "name" : "name",
+    "location" : {
+      "date" : [ "2000-01-23T04:56:07.000+00:00", "2000-01-23T04:56:07.000+00:00" ],
+      "coords" : [ [ 0.8008281904610115, 0.8008281904610115 ], [ 0.8008281904610115, 0.8008281904610115 ] ]
+    },
+    "status" : true,
+    "measurements" : {
+      "date" : [ "2000-01-23T04:56:07.000+00:00", "2000-01-23T04:56:07.000+00:00" ],
+      "rain" : [ "2000-01-23T04:56:07.000+00:00", "2000-01-23T04:56:07.000+00:00" ],
+      "light" : [ 2.3021358869347655, 2.3021358869347655 ],
+      "temperature" : [ 1.4658129805029452, 1.4658129805029452 ],
+      "humidity" : [ 7.061401241503109, 7.061401241503109 ],
+      "pressure" : [ 6.027456183070403, 6.027456183070403 ],
+      "wind" : {
+        "speed" : [ 5.962133916683182, 5.962133916683182 ],
+        "direction" : [ 5.637376656633329, 5.637376656633329 ]
+      }
+    }
+  };
+}
+
+
 
 /* GET Archive data */
-router.get('/archive', async function(req, res, next) {
-  const { from, to, filter, interval } = req.query;
-  
+router.get('/', async function(req, res, next) {
   try {
-    const archiveData = await getArchiveData(from, to, filter, interval);
+    const archiveData = await getArchiveData();
     res.json(archiveData);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
