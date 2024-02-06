8b073eefb583a57993bd9ded88a2dd3eeab30f61
Claire_GRD <claire-girardin@hotmail.fr>
2024-02-06 17:27:14 +0100

    server ok 6 pages

--- Server/routes/coffee.js
+++ Server/routes/coffee.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(418).send('GetCoffee : Nope, pas dispo ...');
 });
 
 module.exports = router;
--- Server/routes/pirate.js
+++ Server/routes/pirate.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(404).send("C'est sécure !!");
 });
 
 module.exports = router;
