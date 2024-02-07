9a2bced9f01d1f87cc8fecb3a941a26bede0f8dc
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-02-07 16:51:26 +0100

    correct installer

--- Install/setup_pi.sh
+++ Install/setup_pi.sh
@@ -1,11 +1,14 @@
 pipass=raspberry;
 
-sshpass -p $pipass ssh pi@piensg028 "mkdir /home/pi/station && echo 'INFLUXDB_TOKEN=***' > /home/pi/station/.etc";
+echo "init repository...";
+sshpass -p $pipass ssh pi@piensg028 "mkdir /home/pi/station && echo 'INFLUXDB_TOKEN=***' > /home/pi/station/.env";
 
+echo "transfer files...";
 sshpass -p $pipass scp -r DataRecorder/ pi@piensg028:/home/pi/station;
 sshpass -p $pipass scp -r Server/ pi@piensg028:/home/pi/station;
-sshpass -p $pipass scp -r Service/ pi@piensg028:/home/pi/station;
+sshpass -p $pipass scp -r Services/ pi@piensg028:/home/pi/station;
 
+echo "start container & services...";
 sshpass -p $pipass ssh pi@piensg028 "\
 sudo cp /home/pi/station/Service/*.service /etc/systemd/system/ && \
 sudo systemctl start meteoapi.service && \
@@ -13,4 +16,4 @@ sudo systemctl start meteodata.service && \
 sudo systemctl enable meteoapi.service && \
 sudo systemctl enable meteodata.service";
 
-echo "setup terminé. Terminez l'installation de la base de données et renseignez un tocken R&W dans /home/pi/station/.etc, puis executez le script update_pi.sh";
+echo "done\nsetup terminé. Terminez l'installation de la base de données et renseignez un tocken R&W dans /home/pi/station/.etc, puis executez le script update_pi.sh";
--- Install/update_pi.sh
+++ Install/update_pi.sh
@@ -1,13 +1,15 @@
 pipass=raspberry;
 
+echo "transfer files...";
 sshpass -p $pipass scp -r DataRecorder/ pi@piensg028:/home/pi/station;
 sshpass -p $pipass scp -r Server/ pi@piensg028:/home/pi/station;
-sshpass -p $pipass scp -r Service/ pi@piensg028:/home/pi/station;
+sshpass -p $pipass scp -r Services/ pi@piensg028:/home/pi/station;
 
+echo "restart services...";
 sshpass -p $pipass ssh pi@piensg028 "\
-sudo cp /home/pi/station/Service/*.service /etc/systemd/system/ && \
+sudo cp /home/pi/station/Services/*.service /etc/systemd/system/ && \
 sudo systemctl daemon-reload && \
 sudo systemctl restart meteoapi.service && \
 sudo systemctl restart meteodata.service";
 
-echo "Update terminé";
+echo "Update done";
