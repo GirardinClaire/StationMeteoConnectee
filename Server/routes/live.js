aa75b928946d07f2a3459feb5b96c8edc400d7b3
VGiudicelli <vincent.giudicelli@ensg.eu>
2024-02-07 14:31:40 +0100

    link liveData from DataRecorder

--- Server/routes/live.js
+++ Server/routes/live.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendFile('liveData.json', { root: '../Server' })
+//  res.sendFile('liveData.json', { root: '../Server' })
+  res.sendFile('live.json', { root: '../DataRecorder/Data' })
 });
 
 module.exports = router;
